7654cdf7e2e0c9d5471a1fbe8745be0bdb8322b0
Ronit Panda <pandaronit25@gmail.com>
2021-08-25 16:42:24 +0530

    added a feature that can fock you hard

--- frontend/lib/screens/feed.dart
+++ frontend/lib/screens/feed.dart
@@ -4,6 +4,7 @@ import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 import 'package:provider/provider.dart';
 import 'package:tiktoklikescroller/tiktoklikescroller.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class QuestionFeed extends StatefulWidget {
   @override
@@ -16,6 +17,7 @@ class _QuestionFeedState extends State<QuestionFeed> {
     return FutureBuilder(
         future: Provider.of<Data>(context, listen: false).getFeedCards(),
         builder: (context, snapshot) {
+          // BBAyRBTfsOU
           if (snapshot.connectionState == ConnectionState.done) {
             print(
                 Provider.of<Data>(context, listen: false).feedCards.toString() +
@@ -27,77 +29,166 @@ class _QuestionFeedState extends State<QuestionFeed> {
                 animationDuration: const Duration(milliseconds: 250),
                 builder: (BuildContext context, int index) {
                   print(Provider.of<Data>(context).feedCards.length);
-                  return Card(
-                    margin: EdgeInsets.all(20.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    elevation: 20,
-                    shadowColor: Colors.black,
-                    color: Color(0xFF111328),
-                    child: Column(
-                      children: [
-                        if (Provider.of<Data>(context, listen: false)
-                                .feedCards[index]
-                                .imageUrl ==
-                            'no image')
-                          SizedBox(height: 0)
-                        else
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
+                  if (Provider.of<Data>(context).feedCards[index].isVideo ==
+                      false) {
+                    return Card(
+                      margin: EdgeInsets.all(20.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      elevation: 20,
+                      shadowColor: Colors.black,
+                      color: Color(0xFF111328),
+                      child: Column(
+                        children: [
+                          if (Provider.of<Data>(context, listen: false)
+                                  .feedCards[index]
+                                  .imageUrl ==
+                              'no image')
+                            SizedBox(height: 0)
+                          else
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(30.r),
+                                  topRight: Radius.circular(30.r),
+                                ),
+                                child: Image(
+                                  fit: BoxFit.fitWidth,
+                                  height: 300.h,
+                                  width: double.infinity,
+                                  image: NetworkImage(
+                                    Provider.of<Data>(context, listen: false)
+                                        .feedCards[index]
+                                        .imageUrl,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                child: Text(
+                                  Provider.of<Data>(context, listen: false)
+                                      .feedCards[index]
+                                      .heading,
+                                  style: TextStyle(
+                                    fontSize: 25.sp,
+                                    color: Color(0xFFEB1555),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 20.h,
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                                child: Text(
+                                  Provider.of<Data>(context, listen: false)
+                                      .feedCards[index]
+                                      .content
+                                      .replaceAll("20", '\n\n\u2022 '),
+                                  style: TextStyle(
+                                    fontSize: 17.sp,
+                                    color: Colors.white60,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  } else {
+                    return Card(
+                      margin: EdgeInsets.all(20.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      elevation: 20,
+                      shadowColor: Colors.black,
+                      color: Color(0xFF111328),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Container(
+                            height: 350.h,
+                            width: 350.w,
                             child: ClipRRect(
                               borderRadius: BorderRadius.only(
                                 topLeft: Radius.circular(30.r),
                                 topRight: Radius.circular(30.r),
                               ),
-                              child: Image(
-                                fit: BoxFit.fitWidth,
-                                height: 300.h,
-                                width: double.infinity,
-                                image: NetworkImage(
-                                  Provider.of<Data>(context, listen: false)
-                                      .feedCards[index]
-                                      .imageUrl,
+                              child: YoutubePlayer(
+                                bottomActions: [],
+                                aspectRatio: 16 / 9,
+                                controller: YoutubePlayerController(
+                                  initialVideoId: YoutubePlayer.convertUrlToId(
+                                      Provider.of<Data>(context, listen: false)
+                                          .feedCards[index]
+                                          .content)!, //Add videoID.
+                                  flags: YoutubePlayerFlags(
+                                    hideControls: false,
+                                    controlsVisibleAtStart: true,
+                                    autoPlay: false,
+                                    mute: false,
+                                  ),
                                 ),
+                                showVideoProgressIndicator: true,
+                                progressIndicatorColor: Color(0xFFEB1555),
                               ),
                             ),
                           ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                              child: Text(
-                                Provider.of<Data>(context, listen: false)
-                                    .feedCards[index]
-                                    .heading,
-                                style: TextStyle(
-                                  fontSize: 25.sp,
-                                  color: Color(0xFFEB1555),
+                          SizedBox(
+                            height: 30.h,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                child: Text(
+                                  Provider.of<Data>(context, listen: false)
+                                      .feedCards[index]
+                                      .heading,
+                                  style: TextStyle(
+                                    fontSize: 25.sp,
+                                    color: Color(0xFFEB1555),
+                                  ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              height: 20.h,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-                              child: Text(
-                                Provider.of<Data>(context, listen: false)
-                                    .feedCards[index]
-                                    .content
-                                    .replaceAll("20", '\n\n\u2022 '),
-                                style: TextStyle(
-                                  fontSize: 17.sp,
-                                  color: Colors.white60,
+                              SizedBox(
+                                height: 20.h,
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                                child: Text(
+                                  Provider.of<Data>(context, listen: false)
+                                      .feedCards[index]
+                                      .desco
+                                      .replaceAll("20", '\n\n\u2022 '),
+                                  style: TextStyle(
+                                    fontSize: 17.sp,
+                                    color: Colors.white60,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  );
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  }
                 });
           } else {
             return Center(
