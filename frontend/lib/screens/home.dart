6dc36edc0d16116f25bfe62cb20bfa1ef6663fb5
Aditya Sinha <sinhadityaforever@gmail.com>
2021-08-31 17:08:28 +0530

    abcd

--- frontend/lib/screens/home.dart
+++ frontend/lib/screens/home.dart
@@ -25,6 +25,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    Provider.of<Data>(context, listen: false).getLikedCards();
     Provider.of<Data>(context, listen: false).getBookMark();
     for (var i = 0;
         i < Provider.of<Data>(context, listen: false).roomId.length;
